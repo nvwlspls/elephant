8fd8082e8b215b9ad5241923f2132b09dd80a15e
wayne jessen <mremeat@gmail.com>
2015-05-05 20:58:50 -0700

    add and remove band works

--- static/addShow/addShow.js
+++ static/addShow/addShow.js
@@ -9,6 +9,23 @@ angular.module('moogitShows.addShow', ['ngRoute'])
   });
 }])
 
-.controller('addShowCtrl', [function() {
+.controller('addShowCtrl', ['$scope', function($scope) {
+        $scope.bands = [
+            {
+                id : ''
+            },{
+                id :''
+            }
+        ]
 
-}]);
+        $scope.addNewBand = function() {
+            $scope.bands.push({
+                id :''
+            })
+        }
+
+        $scope.removeBand = function(bandIndex) {
+            $scope.bands.splice(bandIndex, 1)
+        }
+
+    }])
