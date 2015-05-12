9dde16033be9331dcf2300e31846139e02a0c683
wayne jessen <mremeat@gmail.com>
2015-05-11 23:30:45 -0700

    Auto complete is working on enter show page!

--- static/home/homeservice.js
+++ static/home/homeservice.js
@@ -2,13 +2,32 @@
  * Created by waynejessen on 5/9/15.
  */
 
-moogitShows.factory('getshows', ['$http', function($http){
-               return $http.get('/shows/getshows')
-                   .success(function(data){
-                       return data;
-                   })
-                   .error(function(err){
-                       return err;
-                   })
-                }
-]);
+//moogitShows.factory('getshows', [ '$http', function($http) {
+//
+//
+//
+//            return $http.get("/shows/getshows/9")
+//                .success(function(data) {
+//                    return data;
+//                })
+//                .error(function(err) {
+//                    return err;
+//                })
+//        }
+//]);
+
+
+moogitShows.factory('getshows', function($http){
+
+    var Getshows = function(){
+        this.items = function[];
+        this.busy = false;
+        this.after = '';
+    }
+
+    Getshows.prototype.nextPage = function(){
+        if (this.busy) return;
+        this.busy = true;
+        var url = "/shows/getshows" + this.after
+    }
+})
